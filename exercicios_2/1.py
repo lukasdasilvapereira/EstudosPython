ebdc7c9020cc9f4018b0e3c1174b3fa9e2c7b48e
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-10-01 23:01:44 -0300

    Create 1.py

---
+++ exercicios_2/1.py
@@ -0,0 +1,32 @@
+nomes = ["Ana", "Lucas", "Bruno"]
+idades = [21, 19, 22]
+
+print(list(zip(nomes, idades)))
+
+#from intertools import zip_longest
+
+alunos = ["Lucas", "Anderson", "Gabriele", "Vitor"]
+notas = [8.5, 9.0, 5.0]
+
+print(list(zip(alunos, notas)))
+
+# SOMA
+
+a = [1, 2, 3]
+b = [10, 20, 30]
+nova_lista = []
+
+for n, c in zip(a, b):
+    nova_lista.append(n + c)
+
+print(nova_lista)
+
+# Times
+from itertools import zip_longest
+
+estados = ["São Paulo", "Rio de Janeiro", "Belo Horizonte"]
+
+times = ["Palmeiras", "Flamengo"]
+
+print(list(zip_longest(estados, times, fillvalue="N/A")))
+
